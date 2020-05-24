5537bf322eeaf755dd9b66850f509103d9c4c498
Dora Gera <geradora99@gmail.com>
2020-05-24 12:02:04 +0200

    added mock object

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,16 +9,20 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class); 
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(primary.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
@@ -29,7 +33,10 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(primary.isEmpty()).thenReturn(false);
+    when(secondary.isEmpty()).thenReturn(false);
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
